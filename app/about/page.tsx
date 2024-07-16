98725faab04c29ae6d702e61dcedb8a0ee47e8f6
muhammad nur rahman <muhammad@localhost.localdomain>
2024-07-17 06:31:47 +0700

    change font and add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,8 +9,9 @@ export default function Home() {
   return (
     <main className={`flex h-screen min-h-screen flex-col items-center justify-center ${roboto_mono.className}`}>
       <div className="w-96 text-center text-sm rounded border-2 p-2 border-slate-200 bg-slate-100 justify-center item-center">
-      <p>Ini adalah website kumpulan status bergambar untuk muslim, semoga bermanfaat dan menambah sedikit pengetahuan.</p>
+        <p>Github :  </p>
       </div>
     </main>
   );
 }
+
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
+import { Kanit } from "next/font/google";
 
 const menus = [
   {
@@ -12,19 +13,28 @@ const menus = [
     link: "/gallery",
     name: "Gallery",
   },
+  {
+    link: "/about",
+    name: "About",
+  },
 ];
+
+const kanit = Kanit({
+  subsets:['latin'],
+  weight:['400','700']
+});
 
 export default function NavBar() {
   const current = usePathname();
   return (
-    <div className="flex bg-slate-500 gap-3">
+    <div className={`flex bg-slate-500 gap-3 ${kanit.className}`}>
       {menus.map((menu) => {
         return (
           <Link
             key={menu.name}
             href={menu.link}
             className={clsx("text-white p-3", {
-              "bg-yellow-500 font-bold": current === menu.link,
+              "bg-yellow-500": current === menu.link,
             })}
           >
             {menu.name}
