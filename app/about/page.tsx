4587431fa1acfabc94d6d65b964f6f7e0627754d
muhammad nur rahman <muhammad@localhost.localdomain>
2024-07-22 21:15:55 +0700

    edit content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className={`flex h-screen min-h-screen flex-col items-center justify-center ${roboto_mono.className}`}>
       <div className="w-96 text-center text-sm rounded border-2 p-2 border-slate-200 bg-slate-100 justify-center item-center">
         <p><Link href="https://github.com/mynameismaman/collections-status.git" rel="noopener noreferrer" target="_blank">Menuju Repo Github</Link></p><br/>
-        <p>Jika ada gambar/karya yang tidak boleh di publish pada website ini, silahkan hubungi admin@samaun.my.id</p>
+        <p>Jika terdapat gambar/karya anda yang tidak diperbolehkan untuk dipublish, silahkan hubungi admin@samaun.my.id</p>
       </div>
     </main>
   );
