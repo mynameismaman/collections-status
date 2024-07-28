2160f03f449c6d5e8f29d260110a86ca509b3db5
muhammad nur rahman <muhammad@localhost.localdomain>
2024-07-28 10:39:21 +0700

    fix error react/no-unescaped-entities

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <main>{children}</main>
           <footer>
       <div className="text-center text-sm rounded border-2 p-2 border-slate-200 bg-slate-100 justify-center item-center">
-        <p className="text-xs">muslim status | gambar muslim | gambar islam | gambar islami | bid'ah | bidah | kurofat | kurafat | sunnah | salaf | wahabi | jamaah | ahlusunnah | salafi | ahlulsunnah | ahlusunnah wal jamaah | muslim gambar | islami gambar | jalan lurus</p><br/>
+        <p className="text-xs">muslim status | gambar muslim | gambar islam | gambar islami | bid&apos;ah | bidah | kurofat | kurafat | sunnah | salaf | wahabi | jamaah | ahlusunnah | salafi | ahlulsunnah | ahlusunnah wal jamaah | muslim gambar | islami gambar | jalan lurus</p><br/>
         <p>&copy; 2024 samaun.my.id</p>
       </div>
 
